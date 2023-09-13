26b4a6e092f6f4f918b5da7ac9d5ca695a83c5f6
JQiana <luo10@iu.edu>
2023-09-13 13:08:08 -0400

    change

--- arraysStudents.js
+++ arraysStudents.js
@@ -14,3 +14,49 @@ aText.innerHTML += "<br/>Third Array Value" + grades[2];
 let gradeTotal = grades[0]+grades[1]+grades[3];
 aText.innerHTML += "<br/>Array Average:" + (gradetotal/grades.length);
 aText.innerHTML += "<br/>Above Average:" +((gradeTotal/grades.length)>80);
+
+let tops = ['tishirts', 'sweater','sweatchirt','blouse','button-down','hoosie'];
+
+let exText = document.getElementById('extraAnswers');
+
+tops.push('tank');
+exText.innerHTML = "Array Push:" + tops;
+
+//removes last value
+tops.pop();
+exText.innerHTML+="<br/>Array Pop:" + tops;
+
+//remove first value
+top.shift();
+exText.innerHTML+="<br/>Array Shift:" + tops;
+
+//add value at beginning
+top.unshift("T-shirt");
+exText.innerHTML+="<br/>Array Inshift:" + tops;
+
+//Add/Delet values in the middle
+tops.splice(2,1); //Delet sweatshirt
+exText.innerHTML+="<br/>Delet Sweatshirt:" + tops;
+
+tops.splice(3,0,"sweatshirt","tank");
+exText.innerHTML+="<br/>Add Sweatshirt and tank:" + tops;
+
+let bottoms=['jeans','shorts','skirts','slacks','sweatpants'];
+
+let outfits=[tops,bottoms];
+
+document.write(outfits[0][1] + "" + outfits[1][3]);
+
+//OBJECT
+
+const friend = {
+    fName:"william",
+    lName:"Stevens",
+    age:27,
+    studnet:false,
+    fullName: function(){
+        return this.fName + " " + this.lName;
+    }
+}
+
+console.log(friend.fullName()+"is" + friend.age + "years old.");
