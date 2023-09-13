a5ae1e863f9395899bbb3401ed4e2863e6baed62
JQiana <luo10@iu.edu>
2023-09-13 12:32:26 -0400

    change

--- arraysStudents.js
+++ arraysStudents.js
@@ -9,4 +9,8 @@ aText.innerHTML += "<br/>Array Length:" + grades.length;
 let lastGrade = grades.length-1; //result will be 3
 let lgValue = grades[lastGrade]++;
 aText.innerHTML += "<br/>Last Grade Change:" + lgValue;
-
+aText.innerHTML += "<br/>Array:" + grades;
+aText.innerHTML += "<br/>Third Array Value" + grades[2];
+let gradeTotal = grades[0]+grades[1]+grades[3];
+aText.innerHTML += "<br/>Array Average:" + (gradetotal/grades.length);
+aText.innerHTML += "<br/>Above Average:" +((gradeTotal/grades.length)>80);
