1d4309303b2dae2dd1376dffc2ef49018333125e
JQiana <luo10@iu.edu>
2023-09-13 16:15:29 -0400

    change

--- homework-three/InterestCalculator.js
+++ homework-three/InterestCalculator.js
@@ -5,6 +5,7 @@ function calcInterest(principal,rate,time){
     var t = document.getElementById('time').value;
     //var duration = document.getElementById('duration').value;
     let interest = 1 + r * t;
+    let result = p*interest;
 
     document.getElementById('result').
     innerHTML = "With a beginning principal of" + principal + "and with a growth rate of" + rate + "% for" + time + "years, your total interest will be" + simplr + "with a grand total of $" + result;
