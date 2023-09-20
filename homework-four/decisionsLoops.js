f114b9740a34579e933b4ffea53ebf918e87b9b6
JQiana <luo10@iu.edu>
2023-09-20 15:46:05 -0400

    q

--- homework-four/decisionsLoops.js
+++ homework-four/decisionsLoops.js
@@ -3,7 +3,5 @@ let toy = 45.99;
 let buyToy = bank>toy; //t/f
 //bank small than toy so it has nothing show uo\p
 if(!buyToy){
-    document.getElementById('banl').innerHTML = "Let's go buy it!";
-}else{
-    document.getElementById('banl').innerHTML = "Better save for that toy!";
+    document.getElementById('bank').innerHTML = "Let's go buy it!";
 }
