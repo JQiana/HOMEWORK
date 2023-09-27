de59a67661103a8b5dd77a61d672e9be60479a3e
JQiana <luo10@iu.edu>
2023-09-27 12:39:00 -0400

    k

--- homework-four/decisionsLoops.js
+++ homework-four/decisionsLoops.js
@@ -32,3 +32,40 @@ function checkAge(){
 
     document.getElementById('voteAge').innerHTML = ageText;
 }
+//greeting
+let tHours = today.getHours();
+let greeting;
+tHours = 18;
+if(tHours<=12){
+    greeting = "Good Morning!";
+}else if (myHours>16 && myHours<=20){
+    greeting = "Googd Evening!";
+}else{
+    greeting = "Good Noght";
+}
+document.getElementById('myTime').innerHTML = greeting;
+
+//student
+let midterm = 78;
+let final = 87;
+let total = midterm+final+47+62;
+let myAvg = (total/350)*100;
+var studentText;
+
+if(midterm>50 && final>72){
+    studentText = "You passed the class";
+}else {
+    studentText = "Sorru - You did not pass the class";
+}
+
+if((midterm>50 && final>72) || myAvg>80){
+    studentText+="<br/>The class is not complete";
+}
+
+document.getElementById('msg').innerHTML = studentText;
+
+//Random Number
+function newNum(){
+    let rNum = Math.floor(Math.random()*20)+1;
+    document.getElementById('rNum20').innerHTML = rNum;
+}
