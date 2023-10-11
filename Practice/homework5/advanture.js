6960f338e8b27e8efbf63a33015d5dec927ec58d
JQiana <luo10@iu.edu>
2023-10-11 15:38:27 -0400

    g

---
+++ Practice/homework5/advanture.js
@@ -0,0 +1,4 @@
+function startGame(){
+    let player = prompt("Welcome to my choose-your-own advanture game.Please start by entering your name:");
+    alert("Welcome," + player + "! Today we are going on a campus adventure? Are you ready?Click ok to begin.");
+}
